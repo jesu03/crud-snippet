bd2b73c5d00444160ca108097c5987f23a0feb9b
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-10 13:43:03 +0530

    make an controller class

--- snippet/src/main/java/com/example/snippet/Classes/CustomerController.java
+++ snippet/src/main/java/com/example/snippet/Classes/CustomerController.java
@@ -3,9 +3,13 @@ package com.example.snippet.Classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
-    List<Customer> customer = new ArrayList<>();    
+    List<Customer> customer = new ArrayList<>(); 
+
+    
 }
