15f13183e8373734d7aae09c05c3b65d0d5810f3
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-10 14:25:36 +0530

    switch to putmapping

--- snippet/src/main/java/com/example/snippet/Classes/CustomerController.java
+++ snippet/src/main/java/com/example/snippet/Classes/CustomerController.java
@@ -2,8 +2,13 @@ package com.example.snippet.Classes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,22 @@ public class CustomerController {
     @GetMapping(value="/")
     public List<Customer> getAll(){
         return customer;
+    }
+    @PostMapping(value="/")
+    public Customer persist(@RequestBody Customer custo){
+           Customer cus = new Customer(custo.custname,custo.custemail,custo.custphone,custo.custcity);
+           customer.add(cus);
+           return cus;
+    }
+    @PutMapping(value="/info/{id}")
+    public Customer put(@PathVariable Long id,@RequestBody Customer custo){
+        for(int i=0;i<customer.size();i++){
+            if(Objects.equals(customer.get(i).id,id)){
+                    Customer updatedata = custo;
+                    updatedata.id = id;
+                    customer.set(i, updatedata);
+            }
+        }
+        return custo;
     }
 }
